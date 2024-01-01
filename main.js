7632392d703875eb5f280ef9c57ac527637d1e61
Shatakshi <shatakshiranjan2708@gmail.com>
2024-01-01 13:48:46 +0530

    first-commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function startSudoku() {
+    // Redirect or perform action for starting Sudoku game
+    // You can add your Sudoku game logic here
+    alert('Starting Sudoku!');
+    window.location.href = 'sudoku.html';
+  }
+  
+  function startRPS() {
+    // Redirect or perform action for starting Rock-Paper-Scissors game
+    // You can add your RPS game logic here
+    alert('Starting Stone Paper Scissors!');
+    window.location.href = 'rps.html';
+  }
+  function startTicTacToe() {
+    // Redirect or perform action for starting Tic Tac Toe game
+    // You can add your Tic Tac Toe game logic here
+    alert('Starting Tic Tac Toe!');
+  }
+  
